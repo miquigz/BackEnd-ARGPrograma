91b616cf414d181d23d3431f3b169ad0bdc1a2c8
Miqueas Gimenez <miquigz02@gmail.com>
2022-10-05 10:45:12 -0300

    Commit inicial

---
+++ src/main/java/com/portfolio/miquigz/MiquigzApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.miquigz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiquigzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiquigzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/miquigz/MiquigzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.miquigz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiquigzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
